b77373f62de812a0666cb63b47c3470f08e45589
Timofey Tipishev <tipishev@gmail.com>
2019-03-02 19:21:48 +0100

    add script for commit generation

---
+++ generate_commits.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for i in `seq 501 1000`;
+do
+  # printf "Hello World\n<!--$i-->" > bisect/index.html
+  printf "¡Hola, mundo!\n<!--$i-->" > bisect/index.html
+  git commit -m "add filler commit $i" -- bisect/index.html
+done
