a0e211b73e02ea53417ba090fec758799056cc06
Timofey Tipishev <tipishev@gmail.com>
2019-03-14 19:07:37 +0100

    add auto testing demo

--- bisect_test.sh
+++ bisect_test.sh
@@ -5,8 +5,6 @@ BAD=1
 CANNOT_TEST=125
 
 html=$(cat bisect/index.html)
-# echo "$html"
-# TODO use case/esac?
 if [[ $html == *"mundo"* ]]; then
   exit $((BAD))
 elif [[ $html == *"World"* ]]; then
