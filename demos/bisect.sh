d15d3f89debd6489f9e870c29cc6ecbcd331fc20
Timofey Tipishev <tipishev@gmail.com>
2019-03-14 18:50:55 +0100

    update prompt

--- demos/bisect.sh
+++ demos/bisect.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 . demo-magic.sh  # get your own at https://github.com/dzimine/demo-magic
 
-DEMO_PROMPT="${PURPLE}(${GREEEN}bi|${RED}sect)${GREEN}➜ "
+DEMO_PROMPT="${PURPLE}(${GREEEN}bi${WHITE}|${RED}sect)${GREEN}➜ "
 clear
 
 cd ..
@@ -9,7 +9,6 @@ pe "cat bisect/index.html"
 pe "git status"
 pe "git show 41ff7"
 pe "git log 41ff7..HEAD --oneline"
-pe "git bisect start"
 pe "git bisect bad"
 pe "git bisect good 41ff7"
 pe "cat bisect/index.html"
