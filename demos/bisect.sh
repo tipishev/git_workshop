2b9a27a2efc1846ea51bd9010f468bc6fa360b47
Timofey Tipishev <tipishev@gmail.com>
2019-03-14 19:56:57 +0100

    corrections

--- demos/bisect.sh
+++ demos/bisect.sh
@@ -8,7 +8,7 @@ cd ..
 pe "git checkout e0246d5"
 pe "cat bisect/index.html"
 pe "git show 180b09c -- bisect/index.html"
-pe "git log 180b09c..e0246d5 --oneline"
+pe "git log 180b09c^..e0246d5 --oneline"
 pe "git bisect start"
 pe "git bisect bad e0246d5"
 pe "git bisect good 180b09c"
@@ -21,15 +21,15 @@ pe "git bisect bad"
 
 git bisect reset
 
-pe "git checkout master"
 p "${RED}git bisect reset"
+pe "git checkout master"
 pe "ls"
 pe "cp bisect_test.sh ~/Desktop/"
 pe "vim ~/Desktop/bisect_test.sh"
 pe "git bisect start"
 pe "git bisect bad"
 pe "git bisect good 41ff7"
-pe "git log 180b09c..HEAD --oneline"
+pe "git log 180b09c..master --oneline"
 pe "git bisect run ~/Desktop/bisect_test.sh"
 
 git bisect reset
