b6344e477e6a14ea94d343b45815bd3c944d4549
Timofey Tipishev <tipishev@gmail.com>
2019-03-16 14:15:10 +0100

    typo

--- demos/filters.sh
+++ demos/filters.sh
@@ -9,11 +9,11 @@ rm -f .gitattributes  # will be re-created as the result
 
 pe "ls -a"
 pe "vim bad.py"
-pe "echo '*.py filter=pep8'"
-pe "git config --global filter.pep8.clean autopep8 -"
+pe "echo '*.py filter=pep8' > .gitattributes"
+pe "git config --global filter.pep8.clean 'autopep8 -'"
 pe "git config --global filter.pep8.smudge cat"
 pe "less ~/.gitconfig"
 pe "git add bad.py"
-pe "gid diff --staged"
+pe "git diff --staged"
 
 rm -f .gitattributes  # will be re-created as the result
