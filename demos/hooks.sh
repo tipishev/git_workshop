382fa48dc0df3e2ca010d1a47b701ae10cd4b5bb
Timofey Tipishev <tipishev@gmail.com>
2019-03-16 12:50:21 +0100

    add hooks demo

---
+++ demos/hooks.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+. demo-magic.sh  # get your own at https://github.com/dzimine/demo-magic
+
+DEMO_PROMPT="${PURPLE}(hooks)${GREEN}↪ "
+clear
+
+cd ..
+rm -f .git/hooks/pre-commit
+cd hooks
+pe "ls"
+pe "vim code.py"
+pe "git commit -p"
+pe "ln pre-commit ../.git/hooks/pre-commit"
+pe "git commit -p"
+pe "vim code.py"
+pe "git commit -p"
+
+# cleanup
+git checkout -- code.py
