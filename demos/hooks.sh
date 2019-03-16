8bc44dac0d6fa3a314a4d106a7b89940ad60cdbe
Timofey Tipishev <tipishev@gmail.com>
2019-03-16 13:00:26 +0100

    add okay.jpg ascii art

--- demos/hooks.sh
+++ demos/hooks.sh
@@ -4,15 +4,41 @@
 DEMO_PROMPT="${PURPLE}(hooks)${GREEN}↪ "
 clear
 
+
+OKAY=$(cat <<-END
+
+░░░░▄▄██▀▀▀▀▀▀▀█▄░░░░░░░
+░░▄▀▀░░░░░░░░░░░░▀█▄░░░░
+░█▀░░░░░░░░░░░░░░░░█▄░░░
+▄█░░░▄▄▄█▄▄░░▄░░░░░░█░░░
+█░░░▀██▀▀▀░██▄█░░░░░▀█░░
+█░░░░░▄░░▀▄▄░░░░▄░░░░▀█░
+█▀░████░░░░████░▀░░░░░█░
+█░░▀███░░░░███▀░░░░░░░█▄
+█░░▄░▀▀░░░░▀▀░░▄░░░░░░██
+▀█░░█▀░▄▄░░█░▀▀░░░░░░░█▀
+░▀█░░░░█▀▀▀▀▀▀▄░░░░░░░█░
+░░█░░░█░░▄▄▄▄░▄█░░░░░▄█░
+░░▀█░░██▀▀██▀▀▀█▀░░░▄█░░
+░░░▀█▄░░░░░░░░▄░░░░▄█░░░
+░░░░░▀▄▄▀██████░░▄█▀░░░░
+░░░░░░░▀█▄▄▄▄▄▄▄█▀░░Okay
+░░░░░░░░░▄▄████▄░░░░░░░░
+
+
+END
+)
+
 cd ..
 rm -f .git/hooks/pre-commit
 cd hooks
 pe "ls"
 pe "vim code.py"
 pe "git commit -p"
-pe "vim pre-commit"
-pe "ln pre-commit ../.git/hooks/pre-commit"
+pe "vim no_python_FIXME.py"
+pe "ln no_python_FIXME.py ../.git/hooks/pre-commit"
 pe "git commit -p"
+p "$OKAY"
 pe "vim code.py"
 pe "git commit -p"
 
