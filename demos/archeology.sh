10e72ef61c13c0c59219ba018aae6a0bac71f299
Timofey Tipishev <tipishev@gmail.com>
2019-03-16 15:23:07 +0100

    finish archeology demo

--- demos/archeology.sh
+++ demos/archeology.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 . demo-magic.sh  # get your own at https://github.com/dzimine/demo-magic
 
-DEMO_PROMPT="${PURPLE}(archeology ${WHITE}⛏️)${GREEN}➜ "
+DEMO_PROMPT="${PURPLE}(archeology${WHITE}🦖💩${GREEN}⛏️${PURPLE} )${GREEN}➜ "
 clear
 
 pe "cd ~/sportamore/sportamore"
 pe "git blame sport/models/supplier.py"
-
+pe "git blame sport/models/supplier.py | grep future_payment_days"
+pe "git log -1 --stat 5aea5484eed"
+pe "git log -S 'future_payment_days = models.PositiveIntegerField('"
+pe "git log -1 --stat 849854f009"
+pe "git show 849854f009"
+pe "git log -S 'future_payment_days  ='"
+pe "git show 130235e38"
+pe "git log -S future_payment_days"
