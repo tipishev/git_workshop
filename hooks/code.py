3e6abb2145ee76be5fd8a8cf677c710057a9aa49
Timofey Tipishev <tipishev@gmail.com>
2019-03-18 23:08:31 +0100

    formatting

--- hooks/code.py
+++ hooks/code.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
 
+
 def add(a, b):
     return a + b
--- hooks/no_python_FIXME.py
+++ hooks/no_python_FIXME.py
@@ -6,12 +6,12 @@ import subprocess
 changed_files = [f.decode() for f in subprocess.check_output(
     ['git', 'diff-index', '--cached', '--name-only', 'HEAD']).split()]
 changed_python_files = [filename for filename in changed_files
-                       if filename.endswith('.py')]
+                        if filename.endswith('.py')]
 
 for python_file in changed_python_files:
     with open(python_file) as f:
         for line_number, line in enumerate(f.readlines(), start=1):
             if 'FIXME' in line:
                 print(f"{python_file}:{line_number}: {line}\n"
-                       "Fix it, wouldn't ya?")
+                      "Fix it, wouldn't ya?")
                 sys.exit(1)
